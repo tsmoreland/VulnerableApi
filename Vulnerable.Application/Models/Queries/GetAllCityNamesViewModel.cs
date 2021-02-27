4f34ddc5045e04a66e6b2e4d1a03bc87e5beb92b
Terry Moreland <tsmoreland@live.com>
2021-02-27 03:21:31 +0000

    fixed a reshaprer warning

--- Vulnerable.Application/Models/Queries/GetAllCityNamesViewModel.cs
+++ Vulnerable.Application/Models/Queries/GetAllCityNamesViewModel.cs
@@ -11,11 +11,11 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // 
 
-using System;
 using System.Collections.Generic;
 
 namespace Vulnerable.Application.Models.Queries
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public sealed class GetAllCityNamesViewModel
     {
         public int Count { get; set; }
